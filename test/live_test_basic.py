9b8f940ffe1f529ec1758a4e98b1abc2a286b0de
Jack Grahl <jack.grahl@gmail.com>
2017-07-18 23:38:44 +0000

    This is the content type that HTML should appear as.

--- test/live_test_basic.py
+++ test/live_test_basic.py
@@ -10,7 +10,7 @@ class TestBasicProxy(unittest.TestCase):
 
     def test_is_http(self):
         r = requests.get("http://localhost:80/")
-        self.assertEqual(r.headers['content-type'], "html")
+        self.assertEqual(r.headers['content-type'], "text/html")
 
     def test_is_json(self):
         r = requests.get("http://localhost:80/json")
