ab1d1ddd9e4c8f3b6fd551f435f0832d44077ca2
Jack Grahl <jack.grahl@gmail.com>
2017-07-19 22:08:33 +0000

    Factor out the things we will test for every server.

---
+++ test/base_test.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class BaseTestCase(object):
+    def test_can_retrieve(self):
+        r = requests.get(self.url + "/")
+        self.assertEqual(r.status_code, 200)
+
+    def test_can_retrieve_json(self):
+        r = requests.get(self.url + "/json")
+        self.assertEqual(r.status_code, 200)
+
+    def test_is_http(self):
+        r = requests.get(self.url + "/")
+        self.assertEqual(r.headers['content-type'], "text/html")
+
+    def test_is_json(self):
+        r = requests.get(self.url + "/json")
+        self.assertEqual(r.headers['content-type'], "application/json")
--- test/live_test_basic.py
+++ test/live_test_basic.py
@@ -1,21 +1,7 @@
 import unittest
 
-import requests
+from base_test import BaseTestCase
 
 
-class TestBasicProxy(unittest.TestCase):
-    def test_can_retrieve(self):
-        r = requests.get("http://localhost:80/")
-        self.assertEqual(r.status_code, 200)
-
-    def test_can_retrieve_json(self):
-        r = requests.get("http://localhost:80/json")
-        self.assertEqual(r.status_code, 200)
-
-    def test_is_http(self):
-        r = requests.get("http://localhost:80/")
-        self.assertEqual(r.headers['content-type'], "text/html")
-
-    def test_is_json(self):
-        r = requests.get("http://localhost:80/json")
-        self.assertEqual(r.headers['content-type'], "application/json")
+class TestBasicProxy(BaseTestCase, unittest.TestCase):
+    url = "http://localhost:80"
