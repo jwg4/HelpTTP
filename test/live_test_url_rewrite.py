57723ade00f6512d4f88b3a33d1a7308f0e7b85a
Jack Grahl <jack.grahl@gmail.com>
2017-07-23 22:37:06 +0000

    Fix indent.

--- test/live_test_url_rewrite.py
+++ test/live_test_url_rewrite.py
@@ -7,10 +7,10 @@ class TestBasicProxy(BaseTestCase, unittest.TestCase):
     url = "http://localhost:80/"
 
     def test_can_retrieve_url(self):
-    url = self.url + "/json"
-    r = requests.get(url)
-    data = r.json()
-    self.assertIn('address', data)
-    self.assertIn('url', data['address'])
-    self.assertEqual(data['address']['url'], url)
+        url = self.url + "/json"
+        r = requests.get(url)
+        data = r.json()
+        self.assertIn('address', data)
+        self.assertIn('url', data['address'])
+        self.assertEqual(data['address']['url'], url)
 
