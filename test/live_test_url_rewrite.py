c15562999a25b1871b777c428a7e34a7770aa885
Jack Grahl <jack.grahl@gmail.com>
2017-10-04 21:39:38 +0000

    The json test needs to compare with the url seen by the web server.

--- test/live_test_url_rewrite.py
+++ test/live_test_url_rewrite.py
@@ -7,9 +7,10 @@ from .base_test import BaseTestCase
 
 class TestBasicProxy(BaseTestCase, unittest.TestCase):
     url = "http://localhost:80/"
+    internal_url = "http://localhost:5555/foo"
 
     def test_can_retrieve_url(self):
-        url = self.url + "/json"
+        url = self.internal_url + "/json"
         r = requests.get(url)
         data = r.json()
         self.assertIn('address', data)
